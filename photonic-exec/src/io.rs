69700d6aebdbed4c068a500aa86424df711506ac
Dustin Frisch <fooker@lab.sh>
2021-10-27 13:33:20 +0200

    exec: simplify interface

---
+++ photonic-exec/src/io.rs
@@ -0,0 +1,89 @@
+use std::io::{Read, Write};
+use std::process::{Child, Command, Stdio};
+use std::time::Duration;
+
+use anyhow::{format_err, Context, Result};
+use byteorder::{BigEndian, WriteBytesExt};
+
+use photonic_core::node::RenderType;
+use photonic_core::scene::NodeBuilder;
+use photonic_core::{Buffer, Node, NodeDecl};
+
+use super::Element;
+use std::mem;
+
+#[cfg_attr(feature = "dyn", derive(serde::Deserialize))]
+pub struct IOExecNodeDecl {
+    pub command: String,
+}
+
+pub struct IOExecNode {
+    buffer: Buffer<Element>,
+    child: Child,
+}
+
+impl NodeDecl for IOExecNodeDecl {
+    type Element = Element;
+    type Target = IOExecNode;
+
+    fn materialize(self, size: usize, _builder: &mut NodeBuilder) -> Result<Self::Target> {
+        let command = shlex::split(&self.command)
+            .ok_or_else(|| format_err!("Invalid command: {}", &self.command))?;
+        let (command, args) = command.split_first().ok_or_else(|| format_err!("Empty command"))?;
+
+        let buffer = Buffer::new(size);
+
+        // Spawn a child process
+        let child = Command::new(command)
+            .args(args)
+            .env("PHOTONIC_SIZE", format!("{}", size))
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::inherit())
+            .spawn()?;
+
+        return Ok(Self::Target {
+            buffer,
+            child,
+        });
+    }
+}
+
+impl<'a> RenderType<'a, Self> for IOExecNode {
+    type Render = &'a Buffer<Element>;
+}
+
+impl Node for IOExecNode {
+    const KIND: &'static str = "exec";
+
+    type Element = Element;
+
+    fn update(&mut self, duration: Duration) -> Result<()> {
+        let stdin = self.child.stdin.as_mut().context("StdIn missing")?;
+        let stdout = self.child.stdout.as_mut().context("StdOut missing")?;
+
+        // Send the duration to the child process
+        stdin
+            .write_u64::<BigEndian>(duration.as_millis() as u64)
+            .context("Failed to write to child")?;
+        stdin.flush().context("Failed to write to child")?;
+
+        // Read the buffer content from child
+        stdout
+            .read_exact(bytemuck::cast_slice_mut(self.buffer.as_mut()))
+            .context("Failed to read from child process")?;
+
+        return Ok(());
+    }
+
+    fn render(&self) -> Result<<Self as RenderType<Self>>::Render> {
+        return Ok(&self.buffer);
+    }
+}
+
+impl Drop for IOExecNode {
+    fn drop(&mut self) {
+        mem::drop(self.child.stdout.take());
+        mem::drop(self.child.stdin.take());
+    }
+}
--- photonic-exec/src/lib.rs
+++ photonic-exec/src/lib.rs
@@ -1,115 +1,30 @@
+#![feature(try_blocks)]
 #![allow(clippy::needless_return)]
 
-use std::io::Write;
-use std::process::{Child, Command, Stdio};
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::time::Duration;
-
-use anyhow::{format_err, Context, Result};
-use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
-
 use photonic_core::color::palette::LinSrgb;
-use photonic_core::color::*;
-use photonic_core::node::{Node, NodeDecl, Render, RenderType};
-use photonic_core::scene::*;
-use shared_memory::{Shmem, ShmemConf};
-use std::slice;
 
-#[repr(C, packed)]
-struct Element {
-    r: u8,
-    g: u8,
-    b: u8,
-}
+pub mod io;
 
-pub struct ExecRenderer<'a> {
-    data: &'a [Element],
-}
+type Element = LinSrgb<u8>;
 
-impl<'a> Render for ExecRenderer<'a> {
-    type Element = RGBColor;
+#[cfg(feature = "dyn")]
+pub mod registry {
+    use photonic_core::boxed::BoxedNodeDecl;
+    use photonic_core::color::RGBColor;
+    use photonic_dyn::builder::NodeBuilder;
+    use photonic_dyn::registry;
+    use photonic_dyn::registry::{Factory, NodeRegistry};
 
-    fn get(&self, index: usize) -> Result<Self::Element> {
-        let element = &self.data[index];
-        return Ok(LinSrgb::<u8>::from_components((element.r, element.g, element.b)).into_format());
-    }
-}
+    pub struct Registry;
 
-pub struct ExecNodeDecl {
-    pub command: String,
-}
-
-pub struct ExecNode {
-    size: usize,
-    child: Child,
-    shm: Shmem,
-}
-
-static ID: AtomicUsize = AtomicUsize::new(0);
-
-impl NodeDecl for ExecNodeDecl {
-    type Element = RGBColor;
-    type Target = ExecNode;
-
-    fn materialize(self, size: usize, _builder: &mut NodeBuilder) -> Result<Self::Target> {
-        let command = shlex::split(&self.command)
-            .ok_or_else(|| format_err!("Invalid command: {}", &self.command))?;
-        let (command, args) = command.split_first().ok_or_else(|| format_err!("Empty command"))?;
-
-        let id = format!("photonic-{}-{}", std::process::id(), ID.fetch_add(1, Ordering::SeqCst));
-
-        // Create shared memory region for color buffer
-        let shm =
-            ShmemConf::new().size(size * 3).os_id(&id).create().expect("Failed to create SHM");
-
-        // Spawn a child process
-        let child = Command::new(command)
-            .args(args)
-            .env("PHOTONIC_SIZE", format!("{}", size))
-            .env("PHOTONIC_PATH", format!("/dev/shm/{}", &id))
-            .stdin(Stdio::piped())
-            .stdout(Stdio::piped())
-            .stderr(Stdio::inherit())
-            .spawn()?;
-
-        return Ok(Self::Target {
-            size,
-            child,
-            shm,
-        });
-    }
-}
-
-impl<'a> RenderType<'a, Self> for ExecNode {
-    type Render = ExecRenderer<'a>;
-}
-
-impl Node for ExecNode {
-    const KIND: &'static str = "exec";
-
-    type Element = RGBColor;
-
-    fn update(&mut self, duration: Duration) -> Result<()> {
-        let stdin = self.child.stdin.as_mut().context("StdIn missing")?;
-
-        let stdout = self.child.stdout.as_mut().context("StdOut missing")?;
-
-        // Send the duration to the child process
-        stdin
-            .write_u64::<BigEndian>(duration.as_millis() as u64)
-            .context("Failed to write to child process")?;
-        stdin.flush().context("Failed to flush to child process")?;
-
-        // Wait for any char from the child to signal it's ready
-        stdout.read_u8().context("Failed to write to child process")?;
-
-        return Ok(());
-    }
-
-    fn render(&self) -> Result<<Self as RenderType<Self>>::Render> {
-        let data = unsafe { slice::from_raw_parts(self.shm.as_ptr() as *const Element, self.size) };
-        return Ok(ExecRenderer {
-            data,
-        });
+    impl NodeRegistry for Registry {
+        fn manufacture<Builder: NodeBuilder>(
+            kind: &str,
+        ) -> Option<Box<dyn Factory<BoxedNodeDecl<RGBColor>, Builder>>> {
+            return Some(match kind {
+                "exec:io" => registry::node::<Builder, crate::io::IOExecNodeDecl>(),
+                _ => return None,
+            });
+        }
     }
 }
