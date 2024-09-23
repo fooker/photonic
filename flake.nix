84a9240c3cea311945500f4bc0e8b83a2718996d
Dustin Frisch <fooker@lab.sh>
2024-09-24 00:06:00 +0200

    Updates

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "photonic - dynamic light controller";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     crane = {
       url = "github:ipetkov/crane";
