c2cc2459f8e5c8fa40897ba3b64ad6b0cec36502
Dustin Frisch <fooker@lab.sh>
2024-10-12 02:26:49 +0200

    core/attr: add scale method

--- core/src/attr/ext.rs
+++ core/src/attr/ext.rs
@@ -1,58 +1,75 @@
 use anyhow::Result;
+use num_traits::Num;
 use std::marker::PhantomData;
+use std::ops;
 
-use crate::{scene, Attr, AttrBuilder, AttrValue, FreeAttrDecl};
+use crate::attr::{Bounded, Bounds};
+use crate::{Attr, AttrBuilder, AttrValue, BoundAttrDecl, FreeAttrDecl, RenderContext};
 
-pub trait FreeAttrDeclExt<V: AttrValue>: FreeAttrDecl<V> + Sized {
-    fn map<F, R>(self, f: F) -> FreeMapAttrDecl<Self, F, V, R>
+pub trait FreeAttrDeclExt<V>: FreeAttrDecl<V> + Sized
+where V: AttrValue
+{
+    fn map<F, R>(self, f: F) -> Map<Self, F, V, R>
     where
         F: Fn(V) -> R,
         V: AttrValue,
         R: AttrValue;
+
+    fn scale(self, scale: V) -> Scale<Self, V>
+    where V: Num;
 }
 
-// pub trait BoundAttrDeclExt: BoundAttrDecl + Sized {
-//     fn map<F, R>(self, f: F) -> BoundMapAttrDecl<Self, F, R>
-//         where
-//             F: Fn(Self::Value) -> R,
-//             R: AttrValue;
-// }
+pub trait BoundAttrDeclExt<V>: BoundAttrDecl<V> + Sized
+where V: AttrValue + Bounded
+{
+    fn scale(self, scale: V) -> Scale<Self, V>
+    where V: Num;
+}
 
 impl<V, Decl> FreeAttrDeclExt<V> for Decl
 where
     V: AttrValue,
     Decl: FreeAttrDecl<V>,
 {
-    fn map<F, R>(self, f: F) -> FreeMapAttrDecl<Self, F, V, R>
+    fn map<F, R>(self, f: F) -> Map<Self, F, V, R>
     where
         F: Fn(V) -> R,
         R: AttrValue,
     {
-        return FreeMapAttrDecl {
+        return Map {
             inner: self,
             f,
             phantom: PhantomData,
         };
     }
+
+    fn scale(self, scale: V) -> Scale<Self, V>
+    where V: Num {
+        return Scale {
+            inner: self,
+            scale,
+            phantom: PhantomData,
+        };
+    }
 }
 
-// impl<Decl> BoundAttrDeclExt for Decl
-//     where Decl: BoundAttrDecl
-// {
-//     fn map<F, R>(self, f: F) -> BoundMapAttrDecl<Self, F, R>
-//         where
-//             F: Fn(Decl::Value) -> R,
-//             R: AttrValue,
-//     {
-//         return BoundMapAttrDecl {
-//             inner: self,
-//             f,
-//         };
-//     }
-// }
+impl<V, Decl> BoundAttrDeclExt<V> for Decl
+where
+    V: AttrValue + Bounded,
+    Decl: BoundAttrDecl<V>,
+{
+    fn scale(self, scale: V) -> Scale<Self, V>
+    where V: Num {
+        return Scale {
+            inner: self,
+            scale,
+            phantom: PhantomData,
+        };
+    }
+}
 
 #[derive(Debug)]
-pub struct FreeMapAttrDecl<Inner, F, V, R>
+pub struct Map<Inner, F, V, R>
 where
     F: Fn(V) -> R,
     V: AttrValue,
@@ -63,18 +80,7 @@ where
     phantom: PhantomData<(V, R)>,
 }
 
-// #[derive(Debug)]
-// pub struct BoundMapAttrDecl<Inner, F, R>
-//     where
-//         Inner: BoundAttrDecl,
-//         F: Fn(Inner::Value) -> R,
-//         R: AttrValue,
-// {
-//     inner: Inner,
-//     f: F,
-// }
-
-impl<Inner, F, V, R> FreeAttrDecl<R> for FreeMapAttrDecl<Inner, F, V, R>
+impl<Inner, F, V, R> FreeAttrDecl<R> for Map<Inner, F, V, R>
 where
     Inner: FreeAttrDecl<V>,
     F: Fn(V) -> R,
@@ -96,30 +102,6 @@ where
     }
 }
 
-// impl<Inner, F, R> BoundAttrDecl for BoundMapAttrDecl<Inner, F, R>
-//     where
-//         Inner: BoundAttrDecl,
-//         F: Fn(Inner::Value) -> R,
-//         R: AttrValue + Bounded,
-// {
-//     type Value = R;
-//     type Attr = MapAttr<Inner::Attr, F, R>;
-//
-//     fn materialize(self, bounds: Bounds<R>, builder: &mut AttrBuilder) -> Result<Self::Attr> {
-//         // let bounds = Bounds::from((
-//         //     (self.f)(bounds.min),
-//         //     (self.f)(bounds.max),
-//         // ));
-//
-//         let inner = builder.bound_attr("inner", self.inner, todo!())?;
-//
-//         return Ok(Self::Attr {
-//             inner,
-//             f: self.f,
-//         });
-//     }
-// }
-
 #[derive(Debug)]
 pub struct MapAttr<Inner, F, V, R>
 where
@@ -140,8 +122,74 @@ where
     V: AttrValue,
     R: AttrValue,
 {
-    fn update(&mut self, ctx: &scene::RenderContext) -> R {
+    fn update(&mut self, ctx: &RenderContext) -> R {
         let value = self.inner.update(ctx);
         return (self.f)(value);
+    }
+}
+
+#[derive(Debug)]
+pub struct Scale<Inner, V>
+where V: AttrValue
+{
+    inner: Inner,
+    scale: V,
+    phantom: PhantomData<V>,
+}
+
+impl<Inner, V> FreeAttrDecl<V> for Scale<Inner, V>
+where
+    Inner: FreeAttrDecl<V>,
+    V: AttrValue + ops::Mul<Output = V>,
+{
+    const KIND: &'static str = "scale";
+    type Attr = ScaleAttr<Inner::Attr, V>;
+
+    fn materialize(self, builder: &mut AttrBuilder) -> Result<Self::Attr> {
+        return Ok(Self::Attr {
+            inner: builder.unbound_attr("inner", self.inner)?,
+            scale: self.scale,
+            phantom: self.phantom,
+        });
+    }
+}
+
+impl<Inner, V> BoundAttrDecl<V> for Scale<Inner, V>
+where
+    Inner: BoundAttrDecl<V>,
+    V: AttrValue + Bounded + ops::Mul<Output = V> + ops::Div<Output = V>,
+{
+    const KIND: &'static str = "scale";
+    type Attr = ScaleAttr<Inner::Attr, V>;
+
+    fn materialize(self, bounds: Bounds<V>, builder: &mut AttrBuilder) -> Result<Self::Attr> {
+        return Ok(Self::Attr {
+            inner: builder.bound_attr("inner", self.inner, Bounds {
+                min: bounds.min / self.scale,
+                max: bounds.max / self.scale,
+            })?,
+            scale: self.scale,
+            phantom: self.phantom,
+        });
+    }
+}
+
+pub struct ScaleAttr<Inner, V>
+where
+    Inner: Attr<V>,
+    V: AttrValue,
+{
+    inner: Inner,
+    scale: V,
+    phantom: PhantomData<V>,
+}
+
+impl<Inner, V> Attr<V> for ScaleAttr<Inner, V>
+where
+    Inner: Attr<V>,
+    V: AttrValue + ops::Mul<Output = V>,
+{
+    fn update(&mut self, ctx: &RenderContext) -> V {
+        return self.inner.update(ctx) * self.scale;
     }
 }
