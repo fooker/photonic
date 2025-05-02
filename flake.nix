9421575f3d5cd41bd72e1d09ccd06ac13a04c6d6
Dustin Frisch <fooker@lab.sh>
2025-05-02 23:18:34 +0200

    chore(deps): Update nix and cargo deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "photonic - dynamic light controller";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     crane = {
       url = "github:ipetkov/crane";
